6285481ed41256f14b153cfa547e5dad6ea10d0d
rohan12114 <122142354+rohan12114@users.noreply.github.com>
2023-10-18 08:40:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // complete the given function
 
-function palindrome(str){
-	if(str(0))==(str.length)
-     return true;
-
+function isPalindrome(s) {
+  // Remove non-alphanumeric characters and convert the string to lowercase
+  s = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  
+  // Compare the original string with its reverse
+  return s === s.split('').reverse().join('');
 }
-else false;
-module.exports = palindrome
